cc2c27a3fe12d5a0796c920a2881860fd39b29e7
RichardWen1 <richard.wen@rutgers.edu>
2020-09-16 10:10:56 -0400

    First commit - GroceryItem created

---
+++ src/GroceryItem.java
@@ -0,0 +1,16 @@
+public class GroceryItem {
+	private String name;
+	private double price;
+	private boolean taxable;
+	
+	public boolean equals(Object obj) {
+		if (!(obj instanceof GroceryItem)) return false;
+		GroceryItem item = (GroceryItem) obj;
+		if (name.equals(item.name) && price == item.price && taxable == item.taxable) return true;
+		else return false;
+	}
+	
+	public String toString() {
+		return name + ": $" + String.format("%.2f", price) + " : " + (taxable ? "is taxable" : "tax free");
+	}
+}

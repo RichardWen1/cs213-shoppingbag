8f88665848c0b0b216c68e601aa3d554d229eead
RichardWen1 <richard.wen@rutgers.edu>
2020-09-24 18:08:19 -0400

    Moved TestBed main to ShoppingBag according to project desc
    I realized after rereading the assignment that the testbed main is
    expected in ShoppingBag.java, not its own file
    I've moved the main function from TestBed into ShoppingBag and deleted
    the TestBed file

--- src/ShoppingBag.java
+++ src/ShoppingBag.java
@@ -119,4 +119,52 @@ public class ShoppingBag {
 			System.out.println("\u2022" + bag[i].toString());
 		}
 	}
+	
+	/**
+	Tests the add(), remove(), grow(), salesTax() functions of the ShoppingBag class
+	Makes three ShoppingBag instances with several Grocery Items as test cases
+	See TestSpecification.doc document for further details on test cases.
+	*/
+	public static void main(String[] args) {
+		
+		//tests the add() method in ShoppingBag
+		GroceryItem addTestItem = new GroceryItem("chocolate", 3.75, true);
+		ShoppingBag testBag1 = new ShoppingBag();
+		testBag1.add(addTestItem);
+		testBag1.print();
+		
+		//tests the remove() method in ShoppingBag
+		GroceryItem removeTestItem1 = new GroceryItem("chocolate", 3.75, true);
+		System.out.println(testBag1.remove(removeTestItem1));
+		testBag1.add(removeTestItem1);
+		GroceryItem removeTestItem2 = new GroceryItem("strawberry", 3.50, true);
+		System.out.println(testBag1.remove(removeTestItem2));
+		
+		//tests the grow() method in ShoppingBag
+		ShoppingBag testBag2 = new ShoppingBag();
+		GroceryItem growTestItem1 = new GroceryItem("chocolate", 3.75, true);
+		GroceryItem growTestItem2 = new GroceryItem("strawberry", 3.50, true);
+		GroceryItem growTestItem3 = new GroceryItem("vanilla", 3.75, false);
+		GroceryItem growTestItem4 = new GroceryItem("pistachio", 5.00, false);
+		GroceryItem growTestItem5 = new GroceryItem("blueberry", 4.75, true);
+		GroceryItem growTestItem6 = new GroceryItem("cookie dough", 2.54, true);
+		testBag2.add(growTestItem1);
+		testBag2.add(growTestItem2);
+		testBag2.add(growTestItem3);
+		testBag2.add(growTestItem4);
+		testBag2.add(growTestItem5);
+		testBag2.add(growTestItem6);
+		testBag2.print();
+		
+		//tests the salesTax() method in ShoppingBag
+		ShoppingBag testBag3 = new ShoppingBag();
+		GroceryItem salesTaxTestItem1 = new GroceryItem("chocolate", 3.75, true);
+		GroceryItem salesTaxTestItem2 = new GroceryItem("strawberry", 3.50, true);
+		GroceryItem salesTaxTestItem3 = new GroceryItem("vanilla", 3.75, false);
+		testBag3.add(salesTaxTestItem1);
+		testBag3.add(salesTaxTestItem2);
+		testBag3.add(salesTaxTestItem3);
+		System.out.println(testBag3.salesTax());
+		
+	}
 }

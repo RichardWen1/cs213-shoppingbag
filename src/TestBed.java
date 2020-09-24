34ef7fa40788813725323f3ee6d0d96625f8e58f
Elise-Merritt <36941619+Elise-Merritt@users.noreply.github.com>
2020-09-24 12:53:02 -0400

    tests the add() function

--- src/TestBed.java
+++ src/TestBed.java
@@ -1,5 +1,9 @@
+
 public class TestBed {
 	public static void main(String[] args) {
-		
+		GroceryItem addtestitem = new GroceryItem("chocolate", 3.75, true);
+		ShoppingBag addtestbag = new ShoppingBag();
+		addtestbag.add(addtestitem);
+		addtestbag.print();
 	}
 }

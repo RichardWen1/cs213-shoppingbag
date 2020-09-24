8d8ead4e15c7826c5c529c853402f5fb1f1c18fc
Elise-Merritt <36941619+Elise-Merritt@users.noreply.github.com>
2020-09-24 11:10:00 -0400

    Add Javadoc following software dev rules

--- src/RunProject1.java
+++ src/RunProject1.java
@@ -1,4 +1,12 @@
+/**
+Driver class for running Project 1 by calling Shopping class run method.
+@author Richard Wen, Elise Merritt
+*/
 public class RunProject1 {
+	/**
+	Main method runs the project by creating instance of Shopping class and 
+	using its run method.
+	*/
 	public static void main(String[] args) {
 		new Shopping().run();
 	}
